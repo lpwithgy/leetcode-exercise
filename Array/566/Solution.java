66f8fc801d2a9501a53a15173000232a9190e14c
lpwithgy <paullu666@hotmail.com>
2018-03-15 16:53:04 +0800

    matrixReshape

--- Array/566/Solution.java
+++ Array/566/Solution.java
@@ -2,9 +2,8 @@ class Solution {
     public int[][] matrixReshape(int[][] nums, int r, int c) {
         int rows = nums.length;
         int columns = nums[0].length;
-        if(rows*columns != r*c){
+        if(rows*columns != r*c)
             return nums;
-        }
         int[][] reNums = new int[r][c];
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
